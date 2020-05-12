7b86ffb742533f53871314424567e02a90272e60
IL KWON KIM <magictical@gmail.com>
2020-05-12 12:30:01 +0900

    add custom drawer - main_drawer.dart and display

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../widgets/main_drawer.dart';
 import './favorites_screnn.dart';
 import './categories_screen.dart';
 
@@ -33,6 +34,7 @@ class _TabsScreenState extends State<TabsScreen> {
       appBar: AppBar(
         title: Text(_pages[_selectepageIndex]['title']),
       ),
+      drawer: MainDrawer(),
       body: _pages[_selectepageIndex]['page'],
       bottomNavigationBar: BottomNavigationBar(
         onTap: _selectPage,
